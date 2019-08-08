d079421d4f9460e96b64d20159572a49e51808f0
Ross Mawdsley <rossmawd@gmail.com>
2019-08-08 17:48:46 +0100

    changed playlistIndexCont to a funct component

--- src/Containers/PlaylistsIndexContainer.js
+++ src/Containers/PlaylistsIndexContainer.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import PlaylistComponent from '../Components/PlaylistComponent';
+import { makeStyles } from '@material-ui/core/styles';
 
-class PlaylistsIndexContainer extends Component {
-  render() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    overflow: 'hidden',
+    padding: theme.spacing(0, 0.1), //top and sides of element
+  },
+  paper: {
+    maxWidth: 400,
+    margin: `${theme.spacing(0.2)}px auto`, //space between divs
+    padding: theme.spacing(2), //space between components in div
+    backgroundColor: "grey"
+  },
+}));
+
+ const PlaylistsIndexContainer = (props) => {
+  const classes = useStyles();
     return (
       <div>
-        { this.props.playlists.length !== 0  ? 
-          this.props.playlists.map(playlist => <PlaylistComponent playlist={playlist}/>) : null }
+        { props.playlists.length !== 0  ? 
+          props.playlists.map(playlist => <PlaylistComponent playlist={playlist}/>) : null }
         
       </div>
     );
   }
-}
+
 
 export default PlaylistsIndexContainer;
