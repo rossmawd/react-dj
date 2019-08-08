4c73f0562b29b3b95d24adc90dbf61b9a4270998
Ross Mawdsley <rossmawd@gmail.com>
2019-08-08 11:55:06 +0100

    git test

--- src/Containers/PlayListIndexHeader.js
+++ src/Containers/PlayListIndexHeader.js
@@ -7,6 +7,7 @@ class PlayListIndexHeader extends Component {
   render() {
     return (
       <div>
+        <h1>A little test</h1>
          <ButtonGroup fullWidth aria-label="full width outlined button group">
           <Button>PlayLister</Button>
           <Button>Log Out</Button>
