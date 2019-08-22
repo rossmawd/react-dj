fe2eafc0171936851941f10101ee0f9ee3487e70
Ross Mawdsley <rossmawd@gmail.com>
2019-08-22 15:51:38 +0100

    fixed dodgy filter

--- src/Components/DialogSelect.js
+++ src/Components/DialogSelect.js
@@ -1,50 +1,62 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import InputLabel from '@material-ui/core/InputLabel';
-import Input from '@material-ui/core/Input';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 const useStyles = makeStyles(theme => ({
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap"
   },
   formControl: {
     margin: theme.spacing(1),
-    minWidth: 120,
-  },
+    minWidth: 120
+  }
 }));
 
 export default function DialogSelect(props) {
   const classes = useStyles();
   const [state, setState] = React.useState({
-    genre: '',
+    genre: ""
   });
-  const names = ["All", "New age", "Jazz", "Metal", "Country", "Folk", "Other", "Blues", "Classical", "Electronic", "Rock"] 
+  const names = [
+    "All",
+    "New age",
+    "Jazz",
+    "Metal",
+    "Country",
+    "Folk",
+    "Other",
+    "Blues",
+    "Classical",
+    "Electronic",
+    "Rock"
+  ];
 
   // const handleChange = name => event => {
   //   setState({ ...state, [name]: Number(event.target.value) });
   // };
-const handleChange = (event) => {
-  setState({genre: event.target.value})
-}
+  const handleChange = event => {
+    setState({ genre: event.target.value });
+  };
 
   function handleClickOpen() {
     setState({ ...state, open: true });
   }
 
   const handleChangeFilter = () => {
-     //debugger
-     props.setPlaylistFilter(state.genre)
-     props.toggleFilterForm()
-  }
+    //debugger
+    props.setPlaylistFilter(state.genre);
+    props.toggleFilterForm();
+  };
 
   function handleClose() {
     setState({ ...state, open: false });
@@ -52,27 +64,29 @@ const handleChange = (event) => {
 
   return (
     <div>
-      <Dialog disableBackdropClick disableEscapeKeyDown open={props.showFilterForm} onClose={handleClose}>
+      <Dialog
+        disableBackdropClick
+        disableEscapeKeyDown
+        open={props.showFilterForm}
+        onClose={handleClose}
+      >
         <DialogTitle>Pick A Genre:</DialogTitle>
         <DialogContent>
           <form className={classes.container}>
-           
             <FormControl className={classes.formControl}>
-              <InputLabel htmlFor="genre-simple">genre</InputLabel>
+              <InputLabel htmlFor="genre-simple"></InputLabel>
               <Select
-              native
-                value={state.genre}
+                native
+                value={state.genre === "All" ? null : state.genre}
                 onChange={handleChange}
                 input={<Input id="genre-simple" />}
               >
-        
                 {names.map(name => (
-            <option key={name} value={name}>
-              {name}
-            </option>
-          ))}
+                  <option key={name} value={name}>
+                    {name}
+                  </option>
+                ))}
               </Select>
-
             </FormControl>
           </form>
         </DialogContent>
@@ -80,9 +94,12 @@ const handleChange = (event) => {
           <Button onClick={props.toggleFilterForm} color="primary">
             Cancel
           </Button>
-          <Button onClick={() => {
-            handleChangeFilter()
-          }} color="primary">
+          <Button
+            onClick={() => {
+              handleChangeFilter();
+            }}
+            color="primary"
+          >
             Ok
           </Button>
         </DialogActions>
